e3b4be1ce4d49867b7756720a431e433ce224d86
Brandon Wilson <brandon@coil.com>
2020-04-29 13:51:40 -0500

    webhook scaffolding

--- main.go
+++ main.go
@@ -74,6 +74,10 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "Service")
 		os.Exit(1)
 	}
+	if err = (&corev1.Service{}).SetupWebhookWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create webhook", "webhook", "Service")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	setupLog.Info("starting manager")
